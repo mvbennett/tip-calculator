d6be8b7435e89da49906e45967467a4d0fdd0ff7
Michael Bennett <mvbennett9@gmail.com>
2023-03-05 01:06:11 -0500

    added dollar sign to string

--- tip_calculator.py
+++ tip_calculator.py
@@ -10,6 +10,6 @@ twenty = total * 0.2
 twenty_two = total * 0.22
 
 print(f"""acceptable tips are:
-18%: {eighteen:.2f}
-20%: {twenty:.2f}
-22%: {twenty_two:.2f}""")
+18%: ${eighteen:.2f}
+20%: ${twenty:.2f}
+22%: ${twenty_two:.2f}""")
